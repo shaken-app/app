250cde28150ce3dde5477c9f66e31d95d5d784d7
mattcoady <mattcoady@gmail.com>
2020-03-03 15:33:49 -0800

    S3 Uploader

--- functions/src/upload-to-s3.ts
+++ functions/src/upload-to-s3.ts
@@ -1,4 +1,29 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  console.log("Hello World!");
-  return JSON.stringify(event, null, 2);
+import { v4 as uuidv4 } from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: process.env.REGION || "us-east-1" });
+const s3 = new AWS.S3();
+
+exports.handler = async () => await getUploadURL();
+
+const getUploadURL = async () => {
+  const actionId = uuidv4();
+  const s3Params = {
+    Bucket: "shaken-assets",
+    Key: `${actionId}.jpg`,
+    ContentType: "image/jpeg",
+    ACL: "public-read"
+  };
+  return new Promise(resolve => {
+    let uploadURL = s3.getSignedUrl("putObject", s3Params);
+    resolve({
+      statusCode: 200,
+      isBase64Encoded: false,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({
+        uploadURL: uploadURL,
+        photoFilename: `${actionId}.jpg`
+      })
+    });
+  });
 };
